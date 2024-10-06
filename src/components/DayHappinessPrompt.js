86879d34131df4ac6ac864cb0aab4bb2faa5190f
best-yuriy <bestyuriy@protonmail.com>
2024-10-06 17:04:54 -0500

    feat: added date and date controls to prompt view

--- src/components/DayHappinessPrompt.js
+++ src/components/DayHappinessPrompt.js
@@ -1,4 +1,6 @@
 import './DayHappinessPrompt.css'
+import chevronBackward from '../assets/chevron-backward.svg'
+import chevronForward from '../assets/chevron-forward.svg'
 import ecstatic from '../assets/ecstatic.png'
 import happy from '../assets/happy.png'
 import thoughtful from '../assets/thoughtful.png'
@@ -13,12 +15,20 @@ function HappinessLevel({ percent, altText, src }) {
 
 function DayHappinessPrompt() {
     return (
-        <div id="happiness-level-prompt">
-            <HappinessLevel percent={100} src={ecstatic} altText={":D"}/>
-            <HappinessLevel percent={75} src={happy} altText={":)"}/>
-            <HappinessLevel percent={50} src={thoughtful} altText={":/"}/>
-            <HappinessLevel percent={25} src={sad} altText={":("}/>
-            <HappinessLevel percent={0} src={crying} altText={";("}/>
+        <div id='day-happiness-prompt'>
+            <div id='day-happiness-prompt-header'>Oct 6 2024</div>
+            <div id='day-happiness-prompt-body'>
+                <img id='day-happiness-prompt-left' src={chevronBackward} alt='back'/>
+                <div id='happiness-level-prompt'>
+                    <HappinessLevel percent={100} src={ecstatic} altText={":D"}/>
+                    <HappinessLevel percent={75} src={happy} altText={":)"}/>
+                    <HappinessLevel percent={50} src={thoughtful} altText={":/"}/>
+                    <HappinessLevel percent={25} src={sad} altText={":("}/>
+                    <HappinessLevel percent={0} src={crying} altText={";("}/>
+                    {/* <p>content</p> */}
+                </div>
+                <img id='day-happiness-prompt-right' src={chevronForward} alt='forward'/>
+            </div>
         </div>
     );
 }
