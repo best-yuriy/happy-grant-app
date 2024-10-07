3c4ae1c55b940baf41ec7c3829ba1bef1bf5cfb3
best-yuriy <bestyuriy@protonmail.com>
2024-10-06 21:16:19 -0500

    feat: date control works in prompt view

--- src/components/DayHappinessPrompt.js
+++ src/components/DayHappinessPrompt.js
@@ -6,28 +6,43 @@ import happy from '../assets/happy.png'
 import thoughtful from '../assets/thoughtful.png'
 import sad from '../assets/sad.png'
 import crying from '../assets/crying.png'
+import { useState } from 'react'
+import dayjs from 'dayjs'
 
 function HappinessLevel({ percent, altText, src }) {
     const onClick = () => console.log(`happiness level: ${percent}`);
-    return <img className="happiness-level" src={src} onClick={onClick} alt={altText} />;
-    // return <div className='happiness-level'>{altText}</div>
+    return <img className="happiness-level" src={src} onClick={onClick} alt={altText}/>;
 }
 
 function DayHappinessPrompt() {
+    const [activeDate, setActiveDate] = useState(dayjs().startOf('day'));
+    function addDays(numDays) {
+        const newDate = activeDate.add(numDays, 'day');
+        if (dayjs().startOf('day').isAfter(newDate)) {
+            setActiveDate(newDate);
+        }
+    }
     return (
         <div id='day-happiness-prompt'>
-            <div id='day-happiness-prompt-header'>Oct 6 2024</div>
+            <div id='day-happiness-prompt-header'>{activeDate.format('MMM DD YYYY')}</div>
             <div id='day-happiness-prompt-body'>
-                <img id='day-happiness-prompt-left' src={chevronBackward} alt='back'/>
+                <img id='day-happiness-prompt-left'
+                    src={chevronBackward}
+                    alt='back'
+                    onClick={() => addDays(-1)}
+                />
                 <div id='happiness-level-prompt'>
                     <HappinessLevel percent={100} src={ecstatic} altText={":D"}/>
                     <HappinessLevel percent={75} src={happy} altText={":)"}/>
                     <HappinessLevel percent={50} src={thoughtful} altText={":/"}/>
                     <HappinessLevel percent={25} src={sad} altText={":("}/>
                     <HappinessLevel percent={0} src={crying} altText={";("}/>
-                    {/* <p>content</p> */}
                 </div>
-                <img id='day-happiness-prompt-right' src={chevronForward} alt='forward'/>
+                <img id='day-happiness-prompt-right'
+                    src={chevronForward}
+                    alt='forward'
+                    onClick={() => addDays(1)}
+                />
             </div>
         </div>
     );
