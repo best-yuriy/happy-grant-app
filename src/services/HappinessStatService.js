0f390011d91f9858901365af9eb3c5f70f977ad7
best-yuriy <bestyuriy@protonmail.com>
2024-10-20 14:22:53 -0500

    feat: HappinessStatService.getStatsDaily + refactor to use calendar date strings

--- src/services/HappinessStatService.js
+++ src/services/HappinessStatService.js
@@ -2,37 +2,78 @@ import { getData, putData } from "./LocalStorageService";
 import dayjs from "dayjs";
 
 function _getHappinessStats() {
-    return getData()['happiness-stats'] || [];
+    const stats = (getData()['happiness-stats'] || [])
+    return stats;
+}
+
+function _putHappinessStats(stats) {
+    putData({ 'happiness-stats': stats });
+}
+
+// Input dates in this module are always converted to calendar date strings.
+function _toCalendarDayString(date) {
+    if (typeof date === 'string' || date instanceof String) {
+        return date;
+    }
+    else if (dayjs.isDayjs(date)) {
+        return date.format('YYYY-MM-DD');
+    }
+    else {
+        throw new Error(`Expected dayjs or String, but got ${typeof date}: ${date}.`);
+    }
 }
 
 function getStat(date) {
+    date = _toCalendarDayString(date);
+
     const stats = _getHappinessStats();
-    const stat = stats.find((element) => date.isSame(element.date)) || {};
+    const stat = stats.find(element => element['date'] == date) || {};
     return stat['value'];
 }
 
 function setStat(date, value) {
+    date = _toCalendarDayString(date);
+
     const stats = _getHappinessStats();
-    const index = stats.findIndex((element) => !date.isBefore(dayjs(element['date'])));
+    const index = stats.findIndex(element => element['date'] >= date);
+
     if (index === -1) {
         stats.push({ date, value });
-        putData({ 'happiness-stats': stats });
+        _putHappinessStats(stats);
     }
-    else if (dayjs(stats[index]['date']).isSame(date)) {
+    else if (stats[index]['date'] == date) {
         stats[index]['value'] = value;
-        putData({ 'happiness-stats': stats });
+        _putHappinessStats(stats);
     }
     else {
         const newStats = 
             stats.slice(0, index)
                 .concat([{ date, value }])
                 .concat(stats.slice(index));
-        putData({ 'happiness-stats': newStats });
+        _putHappinessStats(newStats);
     }
 }
 
-function getStatsDaily(startDate, endDate) { throw new Error("unimplemented") }
+function getStatsDaily(startDate, endDate) {
+    startDate = _toCalendarDayString(startDate);
+    endDate = _toCalendarDayString(endDate);
 
-function getStatsWeekly(startDate, endDate) { throw new Error("unimplemented") }
+    const stats = _getHappinessStats();
+
+    const maybeStartIndex = stats.findIndex(element => element['date'] >= startDate);
+    if (maybeStartIndex === -1) return [];
+
+    const maybeEndIndex = stats.findIndex(element => element['date'] >= endDate);
+    const endIndex = maybeEndIndex === -1 ? stats.length : maybeEndIndex;
+
+    return stats.slice(maybeStartIndex, endIndex);
+}
+
+function getStatsWeekly(startDate, endDate) {
+    startDate = _toCalendarDayString(startDate);
+    endDate = _toCalendarDayString(endDate);
+    
+    throw new Error("unimplemented")
+}
 
 export { getStat, setStat, getStatsDaily, getStatsWeekly };
--- src/services/HappinessStatService.test.js
+++ src/services/HappinessStatService.test.js
@@ -1,32 +1,86 @@
-const dayjs = require('dayjs');
 const { getStat, setStat, getStatsDaily, getStatsWeekly } = require('./HappinessStatService')
 
 describe('HappinessStatsService', () => {
 
     beforeEach(() => localStorage.clear());
 
-    const today = dayjs().startOf('day');
+    const day1 = '2024-01-01';
+    const day2 = '2024-01-02';
+    const day3 = '2024-01-03';
+    const day4 = '2024-01-04';
+    const day5 = '2024-01-05';
+    const day6 = '2024-01-06';
 
     it('should return null for a stat that has not been set before', () => {
-        expect(getStat(today)).toBeNull;
+        expect(getStat(day1)).toBeNull;
     })
 
     it('should get and set a single stat', () => {
-        setStat(today, 75);
-        expect(getStat(today)).toBe(75);
+        setStat(day1, 75);
+        expect(getStat(day1)).toBe(75);
     });
 
     it('should set multiple stats in different date order', () => {
-        setStat(today.add(3, 'day'), 50);
-        setStat(today.add(5, 'day'), 100);
-        setStat(today.add(1, 'day'), 0);
-        setStat(today.add(4, 'day'), 75);
-        setStat(today.add(2, 'day'), 25);
+        setStat(day3, 50);
+        setStat(day5, 100);
+        setStat(day1, 0);
+        setStat(day4, 75);
+        setStat(day2, 25);
 
-        expect(getStat(today.add(1, 'day'))).toBe(0);
-        expect(getStat(today.add(2, 'day'))).toBe(25);
-        expect(getStat(today.add(3, 'day'))).toBe(50);
-        expect(getStat(today.add(4, 'day'))).toBe(75);
-        expect(getStat(today.add(5, 'day'))).toBe(100);
+        expect(getStat(day1)).toBe(0);
+        expect(getStat(day2)).toBe(25);
+        expect(getStat(day3)).toBe(50);
+        expect(getStat(day4)).toBe(75);
+        expect(getStat(day5)).toBe(100);
+    });
+
+    it('should update a stat that was previousl set', () => {
+        setStat(day2, 0);
+        
+        // Update the only item in the series.
+        setStat(day2, 25);
+        expect(getStat(day2)).toBe(25);
+
+        // Add the second item to the series.
+        setStat(day1, 0);
+
+        // Update earliest item in the series.
+        setStat(day1, 50);
+        expect(getStat(day1)).toBe(50);
+
+        // Update the latest item in the series.
+        setStat(day2, 75);
+        expect(getStat(day2)).toBe(75);
+
+        // Add the third item to the series.
+        setStat(day3, 0);
+
+        // Update the middle item in the series.
+        setStat(day2, 100);
+        expect(getStat(day2)).toBe(100);
+    });
+
+    it('should get daily stats for the specified interval', () => {
+        setStat(day2, 0);
+        setStat(day3, 25);
+        setStat(day4, 50);
+
+        expect(getStatsDaily(day2, day3))
+            .toEqual([{ date: day2, value: 0 }]);
+        
+        expect(getStatsDaily(day2, day4))
+            .toEqual([{ date: day2, value: 0 }, { date: day3, value: 25 }]);
+        
+        expect(getStatsDaily(day2, day5))
+            .toEqual([{ date: day2, value: 0 }, { date: day3, value: 25 }, { date: day4, value: 50 }]);
+        
+        expect(getStatsDaily(day2, day6))
+            .toEqual([{ date: day2, value: 0 }, { date: day3, value: 25 }, { date: day4, value: 50 }]);
+
+        expect(getStatsDaily(day3, day6))
+            .toEqual([{ date: day3, value: 25 }, { date: day4, value: 50 }]);
+
+        expect(getStatsDaily(day4, day6))
+            .toEqual([{ date: day4, value: 50 }]);
     });
 });
