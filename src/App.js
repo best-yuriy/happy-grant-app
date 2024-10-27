54eb0566043dfee54a6a5288118daa42ba023a3b
best-yuriy <bestyuriy@protonmail.com>
2024-10-27 10:24:50 -0500

    feat: Info view

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import DayHappinessPrompt from './components/DayHappinessPrompt';
 import HappinessStats from './components/HappinessStats';
 import NavHome from './assets/nav-home.svg'
 import NavStats from './assets/nav-stats.svg'
+import NavInfo from './assets/nav-info.svg'
+import Info from './components/Info';
 
 function App() {
   return (
@@ -12,11 +14,13 @@ function App() {
         <nav>
           <Link to="/" className="nav-item"><img src={NavHome} alt='home'/></Link>
           <Link to="/stats" className="nav-item"><img src={NavStats} alt='stats'/></Link>
+          <Link to="/info" className="nav-item"><img src={NavInfo} alt='info'/></Link>
         </nav>
         <div id="main-content">
           <Routes>
             <Route path="/" element={<DayHappinessPrompt />}></Route>
             <Route path="/stats" element={<HappinessStats />}></Route>
+            <Route path="/info" element={<Info />}></Route>
           </Routes>
         </div>
       </BrowserRouter>
---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+import './Info.css'
+
+function Info() {
+    return (
+        <div id='info'>
+            <section>
+                <h1>Oct 27, 2024</h1>
+                <p>This is a preview version of the app. Your data is stored locally in your browser's storage. This means that your data will not transfer from one device to another. Synchronizing data will be added in a future version.</p>
+            </section>
+        </div>
+    );
+}
+
+export default Info;
